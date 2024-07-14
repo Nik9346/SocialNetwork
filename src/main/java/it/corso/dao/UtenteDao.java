93f2b7e1c74515f958e777a1f3a2511ff3b218f8
Nik9346 <nicola.pasquarelli46@gmail.com>
2024-07-14 12:50:35 +0200

    First Commit

--- src/main/java/it/corso/dao/UtenteDao.java
+++ src/main/java/it/corso/dao/UtenteDao.java
@@ -3,7 +3,7 @@ package it.corso.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import it.corso.model.Utente;
-import java.util.List;
+
 
 
 public interface UtenteDao extends CrudRepository<Utente, Integer>{
